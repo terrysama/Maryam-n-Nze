6031718c595a769490d9d1f83b2dbdc51289c9dd
Nnamdi Terry <nwolisannamdi@gmail.com>
2023-04-28 18:42:20 +0100

    Optimized for mobile view and added new images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 (function () {
   const nav = document.querySelector(".my-nav");
   const hero = document.querySelector("#hero");
+  const navScrolled = document.querySelector(".my-nav.scrolled");
+  let navLogo = document.querySelector("#navBrand");
   // const headerCue = document.querySelector(".header-cue");
   // const meetMonsters = document.querySelector("#meet");
   // const monsterImg = document.querySelectorAll(".monster");
@@ -19,6 +21,11 @@
   }, navOptions);
 
   navScrollObserver.observe(hero);
+
+  if (navScrolled) {
+    // navLogo.setAttribute("src", "./assets/img/logo_halo_inverse.svg");
+    navLogo.src = "./assets/img/logo_halo_inverse.svg";
+  }
 
   // /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
   // let prevScrollPosition = window.pageYOffset;
